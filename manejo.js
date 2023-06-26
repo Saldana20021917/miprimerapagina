553c5657024934c7ac0e98fbbf474b27d307485b
Saldana20021917 <saldanajesusdavid@gmail.com>
2023-06-26 18:13:41 -0500

    Initial commit

---
+++ manejo.js
@@ -0,0 +1,7 @@
+const miBoton = document.getElementById("miBoton");
+function handleClick() {
+  alert("Hola este es mi boton");
+}
+miBoton.addEventListener("click",function handleClick() {
+    alert("Hola este es mi boton");
+  });
